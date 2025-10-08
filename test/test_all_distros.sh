e446c3411f00cc2720a4a3b62c04ea64a00ba1bb
Chris Lindbergh <kso512-github@proton.me>
2025-10-08 15:22:35 -0600

    Improve testing script

--- test/test_all_distros.sh
+++ test/test_all_distros.sh
@@ -1,34 +1,54 @@
 #!/usr/bin/env bash
 
 # List of supported distros
-distros=( debian11 debian12 fedora40 fedora41 fedora42 ubuntu2004 ubuntu2204 ubuntu2404 )
+#distros=( debian11 )
+distros=( debian11 debian12 )
+distros+=( fedora40 fedora41 fedora42 )
+distros+=( ubuntu2004 ubuntu2204 ubuntu2404 )
 
 # Fail upon any error
 set -e
 
-# Figure out the last folder in our current path
-current_folder=$(pwd | rev | cut -d"/" -f1 | rev)
-
-# Move to the main role folder if we are in the test folder
-if [ $current_folder == "test" ]; then
-  cd ..
-fi
-
 # Loop through the list of distros
 for distro in ${distros[@]}; do
+
   # Run the docker container, which outputs the unique id of the container
-  echo "--- Running container for distro: $distro"
+  echo "--- Running container for distro: ${distro}"
   container=$(docker run --detach --privileged \
-    --volume=/sys/fs/cgroup:/sys/fs/cgroup:rw --cgroupns=host \
-    --volume=$(pwd):/etc/ansible/roles/role_under_test:ro \
-    geerlingguy/docker-$distro-ansible:latest)
-  # Execute the ansible playbook under test
-  echo "--- Testing role in distro: $distro"
-  docker exec --tty $container env TERM=xterm ansible-playbook \
-    /etc/ansible/roles/role_under_test/test/test.yml
+      --volume=/sys/fs/cgroup:/sys/fs/cgroup:rw --cgroupns=host \
+      --volume=$(pwd):/etc/ansible/roles/role_under_test:ro \
+      "geerlingguy/docker-${distro}-ansible:latest")
+
+  # Run the ansible playbook and look for no failures
+  echo "--- Testing playbook for no failures in distro: ${distro}"
+  ansible_output=$(docker exec --tty "${container}" env TERM=xterm \
+      ansible-playbook /etc/ansible/roles/role_under_test/test/playbook.yml)
+  echo "${ansible_output}" \
+      | grep -o "failed=0"
+
+  # Run the ansible playbook again and look for idempotence
+  echo "--- Testing playbook for no changes (idempotence) in distro: ${distro}"
+  ansible_output=$(docker exec --tty "${container}" env TERM=xterm \
+      ansible-playbook /etc/ansible/roles/role_under_test/test/playbook.yml)
+  echo "${ansible_output}" \
+      | grep -o "changed=0"
+
+  # Make sure the agent runs with sudo as the right user
+  # NOTE: this test assumes the default values are set for:
+  #     checkmk_agent_home
+  #     checkmk_agent_user
+  echo "--- Testing CheckMK agent in distro: $distro"
+  agent_output=$(docker exec --tty $container env TERM=xterm \
+      sudo -u cmkagent sudo /home/cmkagent/check_mk_agent)
+  echo "${agent_output}" \
+      | grep --max-count 1 "\<\<\<check_mk\>\>\>"
+
   # Remove the container once the test passes
-  echo "--- Removing container for distro: $distro"
-  docker rm -f $container
+  echo "--- Removing container for distro: ${distro}"
+  docker rm -f "${container}"
+
+  # End 'for distro' loop
+  echo ""
   done
 
 # Finish clean
